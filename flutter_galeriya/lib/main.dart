3e74a5cb90205bbc8eca6ee8183e5b02e4fbde87
Amal Tom <amaltomsocial@gmail.com>
2024-06-01 02:15:38 +0530

    GridView: builder is used to create a grid of images. ClipRRect is used to apply rounded corners to the images. GestureDetector wraps each thumbnail to handle taps and open the full-screen view.
    Full-Screen Viewer:
    FullScreenViewer is a separate screen that uses CarouselSlider to allow swiping between images.
    initialIndex sets the initially displayed image.
    CarouselOptions configures the carousel slider for infinite scroll, enlarged center page, etc.
    
    Dependencies:
    image_picker: To pick images from the gallery.
    carousel_slider: To implement the swiping functionality for the full-screen image viewer.

--- flutter_galeriya/lib/main.dart
+++ flutter_galeriya/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'dart:io';
 import 'package:flutter/foundation.dart' show kIsWeb;
 
@@ -42,29 +43,82 @@ class _GalleryScreenState extends State<GalleryScreen> {
     }
   }
 
+  void _openFullScreen(int initialIndex) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => FullScreenViewer(
+          images: _images,
+          initialIndex: initialIndex,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter-Galeriya'),
       ),
-      body: GridView.builder(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 3,
-          crossAxisSpacing: 4.0,
-          mainAxisSpacing: 4.0,
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            crossAxisSpacing: 4.0,
+            mainAxisSpacing: 4.0,
+          ),
+          itemCount: _images.length,
+          itemBuilder: (context, index) {
+            return GestureDetector(
+              onTap: () => _openFullScreen(index),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(8.0),
+                child: kIsWeb
+                    ? Image.network(_images[index], fit: BoxFit.cover)
+                    : Image.file(_images[index], fit: BoxFit.cover),
+              ),
+            );
+          },
         ),
-        itemCount: _images.length,
-        itemBuilder: (context, index) {
-          return kIsWeb
-              ? Image.network(_images[index])
-              : Image.file(_images[index]);
-        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _pickImages,
         tooltip: 'Pick Images',
         child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class FullScreenViewer extends StatelessWidget {
+  final List<dynamic> images;
+  final int initialIndex;
+
+  FullScreenViewer({required this.images, required this.initialIndex});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+      ),
+      body: CarouselSlider.builder(
+        itemCount: images.length,
+        options: CarouselOptions(
+          initialPage: initialIndex,
+          enableInfiniteScroll: false,
+          enlargeCenterPage: true,
+          height: double.infinity,
+        ),
+        itemBuilder: (context, index, realIndex) {
+          return Center(
+            child: kIsWeb
+                ? Image.network(images[index], fit: BoxFit.contain)
+                : Image.file(images[index], fit: BoxFit.contain),
+          );
+        },
       ),
     );
   }
